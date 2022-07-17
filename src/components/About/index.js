5b9e49b75b11177c0e511141ed31cfbb7d78d6de
Cameron <sicklecj@gmail.com>
2022-07-17 16:13:41 -0500

    starter files, code, and installs

---
+++ src/components/About/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function About() {
+
+    return (
+        <section>
+            <h1 id="about">Who am I?</h1>
+            <div>
+                <p>
+                    Lorem ipsum dolor sit amet, consectet tristique ies mollis. In hac habitasse platea dictumst.
+                </p>
+            </div>
+        </section>
+    )
+}
+
+export default About
---
+++ src/components/Project/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+// import ProjectList from '../ProjectList';
+import { capitalizeFirstLetter } from '../../utils/helpers';
+
+function Project(props) {
+    // const { currentCategory } = props;
+    return (
+        <section>
+            <h1>projects here</h1>
+        </section>
+    );
+}
+export default Project;
